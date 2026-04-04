cb3a5ff6a12005cf7280efde9420d6224c3fd361
FransiTsena <fransitsena@gmail.com>
2026-04-04 23:15:08 +0300

    feat: update next.config.ts to remove experimental origins and set allowedDevOrigins for dev mode

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // reactCompiler: true,
+  allowedDevOrigins: ["46.225.89.2"],
   devIndicators: {
     appIsrStatus: false,
   },
@@ -11,9 +12,6 @@ const nextConfig: NextConfig = {
     fetches: {
       fullUrl: true,
     },
-  },
-  experimental: {
-    allowedDevOrigins: ["46.225.89.2", "localhost:3000", "localhost:3001"],
   },
   images: {
     qualities: [75, 100],
