a2f53666a0ac477d56024561ce48f50c7c40dc26
FransiTsena <fransitsena@gmail.com>
2026-04-05 00:01:56 +0300

    fix: correct NODE_ENV check and streamline allowedDevOrigins configuration

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,38 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.TYPE === "production";
+const isProd = process.env.NODE_ENV === "test";
 
 const nextConfig: NextConfig = {
   basePath: isProd ? "/hackathon" : "",
   assetPrefix: isProd ? "/hackathon/" : "",
 
-  allowedDevOrigins: [
-    "46.225.89.2:3000",
-    "46.225.89.2:3001",
-    "localhost:3000",
-    "localhost:3001"
-  ],
-
+  /* config options here */
+  // reactCompiler: true,
   devIndicators: {
     appIsrStatus: false,
   },
-
+  // Allow external IP access for dev mode on the Ubuntu server
   logging: {
     fetches: {
       fullUrl: true,
     },
   },
-
+  experimental: {
+    allowedDevOrigins: ["46.225.89.2", "localhost:3000", "localhost:3001"],
+  },
   images: {
     qualities: [75, 100],
     remotePatterns: [
-      { protocol: "https", hostname: "**" },
-      { protocol: "http", hostname: "**" },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
     ],
-  },
+  }
 };
 
 export default nextConfig;
