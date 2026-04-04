34a9576a68c37ff193b96e81e504bf2b4f9919ef
FransiTsena <fransitsena@gmail.com>
2026-04-04 23:12:13 +0300

    feat: update next.config.ts to enhance logging and allow external IP access in dev mode

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // reactCompiler: true,
-  // allowedDevOrigins: ["local-origin.dev"],
+  devIndicators: {
+    appIsrStatus: false,
+  },
+  // Allow external IP access for dev mode on the Ubuntu server
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  experimental: {
+    allowedDevOrigins: ["46.225.89.2", "localhost:3000", "localhost:3001"],
+  },
   images: {
     qualities: [75, 100],
     remotePatterns: [
